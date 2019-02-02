e6f6391eb01fef6d1aa14d7ffdd952f613a2c3eb
Karina <Karina@DESKTOP-T2O8FM8>
2019-02-01 21:22:32 -0500

    Minor changes again on the Main method

--- Assignment2_S19/Program.cs
+++ Assignment2_S19/Program.cs
@@ -50,14 +50,13 @@ namespace Assignment2_S19
             /*{ -20, -3916237, -357920, -3620601,
                7374819, -7330761, 30, 6246457,
                 -6461594, 266854, -520, -470 };*/
-            //
             int[] r4 = closestNumbers(arr3);
             displayArray(r4);
 
             // Day of programmer
             Console.WriteLine("\n\nDay of Programmer");
             int year = 2017;
-            Debug.WriteLine(dayOfProgrammer(year));
+            Console.WriteLine(dayOfProgrammer(year));
         }
 
         static void displayArray(int []arr) {
